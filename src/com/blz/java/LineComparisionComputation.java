6ed25438a197953b29c4d8030c35f9385dc1f68d
Kirandhanwai <kirandhanwai123@gmail.com>
2022-02-16 02:42:17 +0530

    Added : Calculation Line Equality

--- src/com/blz/java/LineComparisionComputation.java
+++ src/com/blz/java/LineComparisionComputation.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 class Point {
     public double x;// co-ordinate x
     public double y;// co-ordinate y
-    public double length;
 
     public double getX() {
 	return x;
@@ -25,17 +24,29 @@ class Point {
 }
 
 public class LineComparisionComputation {
+    static void equalityCheck() {
+	double lineLength1 = lineLength();
+	double lineLength2 = lineLength();
+	if (lineLength1 == lineLength2) {
+	    System.out.println("equal");
+	} else {
+	    System.out.println("not equal");
+	}
+    }
 
     public static double lineLength() {
 	Point point = new Point();
 	Scanner scanner = new Scanner(System.in);
-
+	System.out.println("enter the value of x1");
 	point.setX((scanner.nextInt()));
 	double x1 = point.getX();
+	System.out.println("enter the value of x2");
 	point.setX((scanner.nextInt()));
 	double x2 = point.getX();
+	System.out.println("enter the value of y1");
 	point.setY((scanner.nextInt()));
 	double y1 = point.getY();
+	System.out.println("enter the value of y2");
 	point.setY((scanner.nextInt()));
 	double y2 = point.getY();
 	double x = Math.pow((x1 - x2), 2);
@@ -48,9 +59,7 @@ public class LineComparisionComputation {
 
     public static void main(String[] args) {
 	System.out.println("Welcome to Line Comparison Computation Program using OOPS");
-
-	double lineLength = lineLength();
-	System.out.println("Length of line is " + lineLength);
+	equalityCheck();
 
     }
 	
