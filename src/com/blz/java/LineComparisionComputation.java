8bde513db8905cbfd73054925fb60971747ab58f
Kirandhanwai <kirandhanwai123@gmail.com>
2022-02-16 02:48:33 +0530

    Added : Calculation Line Comparision Using Object Orinted Program

--- src/com/blz/java/LineComparisionComputation.java
+++ src/com/blz/java/LineComparisionComputation.java
@@ -2,79 +2,58 @@ package com.blz.java;
 
 import java.util.Scanner;
 
-class Point {
-    public double x;// co-ordinate x
-    public double y;// co-ordinate y
+class Points {
+    int x1;
+    int y1;
+    int x2;
+    int y2;
 
-    public double getX() {
-	return x;
+    public Points(int x1, int y1, int x2, int y2) {
+	super();
+	this.x1 = x1;
+	this.y1 = y1;
+	this.x2 = x2;
+	this.y2 = y2;
     }
 
-    public void setX(double x) {
-	this.x = x;
-    }
-
-    public double getY() {
-	return y;
-    }
-
-    public void setY(double y) {
-	this.y = y;
-    }
 }
 
 public class LineComparisionComputation {
-    static void equalityCheck() {
-	double lineLength1 = lineLength();
-	double lineLength2 = lineLength();
-	if (lineLength1 == lineLength2) {
-	    System.out.println("equal");
-	} else {
-	    System.out.println("not equal");
-	}
-    }
-
-    static void compareTo() {
-	double lineLength1 = lineLength();
-	double lineLength2 = lineLength();
-	if (lineLength1 == lineLength2) {
-	    System.out.println("lineLength1 is equal to lineLength2");
-	} else if (lineLength1 > lineLength2) {
-	    System.out.println("lineLength1 is greater than lineLength2");
-	} else {
-	    System.out.println("lineLength2 is greater than lineLength1");
-	}
-    }
-
-    public static double lineLength() {
-	Point point = new Point();
-	Scanner scanner = new Scanner(System.in);
-	System.out.println("enter the value of x1");
-	point.setX((scanner.nextInt()));
-	double x1 = point.getX();
-	System.out.println("enter the value of x2");
-	point.setX((scanner.nextInt()));
-	double x2 = point.getX();
-	System.out.println("enter the value of y1");
-	point.setY((scanner.nextInt()));
-	double y1 = point.getY();
-	System.out.println("enter the value of y2");
-	point.setY((scanner.nextInt()));
-	double y2 = point.getY();
-	double x = Math.pow((x1 - x2), 2);
-	double y = Math.pow((y1 - y2), 2);
-	double length = Math.sqrt(x + y);
-	scanner.close();
-
-	return length;
-
-    }
 
     public static void main(String[] args) {
-	System.out.println("Welcome to Line Comparison Computation Program using OOPS");
-	compareTo();
+	System.out.println("Welcome to Line\r\n" + "Comparison Computation\r\n" + "Program on Master Branch");
+	Points points_1 = new Points(1, 2, 5, 8);
+	Points points_2 = new Points(1, 2, 3, 4);
+	LineComparisionComputation lineComp = new LineComparisionComputation();
+	int line1Length = lineComp.lineLengthCal(points_1.x1, points_1.x2, points_1.y1, points_1.y2);
+	int line2Length = lineComp.lineLengthCal(points_2.x1, points_2.x2, points_2.y1, points_2.y2);
+	lineComp.compareTo(line1Length, line2Length);
 
     }
+
+    public int lineLengthCal(int x1, int y1, int x2, int y2) {
+	int lineLength = (int) Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
+	return lineLength;
+    }
+
+    public void equalityCheck(int line1Length, int line2Length) {
+	if (line1Length == line2Length) {
+	    System.out.println("Two lines are equal");
+	} else {
+	    System.out.println("Two lines are not equal");
+	}
+    }
+
+    public void compareTo(int line1Length, int line2Length) {
+	if (line1Length == line2Length) {
+	    System.out.println("Line 1 is equal to Line 2");
+	} else if (line1Length > line2Length) {
+	    System.out.println("Line 1 is greater than to Line 2");
+	} else {
+	    System.out.println("Line 2 is greater than to Line 1");
+	}
+    }
+
 
 	
 }
